fe334dc6f090e91824f0e35df77a74938d9d201a
SvetozarMiskovic <svetomiskovic98@gmail.com>
2021-10-28 18:26:04 +0200

    changes

--- app.js
+++ app.js
@@ -3,52 +3,81 @@ const submitBtn = document.getElementById('submitBtn');
 const createListBtn = document.querySelector('.newListBtn');
 const selectMenu = document.querySelector('select');
 const input = document.querySelector('#todoTask');
-const deleteTaskBtn = document.querySelector('.removeBtn');
+
+function createListElement(listID) {
+  const taskContainer = document.createElement('div');
+  taskContainer.setAttribute('class', 'task-container');
+  taskContainer.setAttribute('data-id', listID);
+  return taskContainer;
+}
+
+function createDeleteListButton(listID) {
+  const removeBtn = document.createElement('i');
+  removeBtn.setAttribute('class', 'fas fa-times-circle removeList');
+  removeBtn.setAttribute('data-id', listID);
+  return removeBtn;
+}
+
+function createTaskList(listID) {
+  const ul = document.createElement('ul');
+  ul.setAttribute('class', 'task-list');
+  ul.setAttribute('data-id', listID);
+  return ul;
+}
+
+function createOption(listID) {
+  const option = document.createElement('option');
+  option.setAttribute('data-id', listID);
+  option.setAttribute('value', listID);
+  option.innerText = 'New list';
+  return option;
+}
 
 function createList() {
   // Create a task container
   let createID = dayjs().valueOf();
-  const taskContainer = document.createElement('div');
-  taskContainer.setAttribute('class', 'task-container');
-  taskContainer.setAttribute('data-id', createID);
-  const removeBtn = document.createElement('i');
-  removeBtn.setAttribute('class', 'fas fa-times-circle removeList');
-  removeBtn.setAttribute('data-id', createID);
-  const ul = document.createElement('ul');
-  ul.setAttribute('class', 'task-list');
-  ul.setAttribute('data-id', createID);
+  const taskContainer = createListElement(createID);
+  const removeBtn = createDeleteListButton(createID);
+  const ul = createTaskList(createID);
+
+  // Add elements to DOM
   taskContainer.appendChild(removeBtn);
   taskContainer.appendChild(ul);
   taskSection.appendChild(taskContainer);
 
   // Create the select item
-
-  const option = document.createElement('option');
-  option.setAttribute('data-id', createID);
-  option.setAttribute('value', option.dataset.id);
-  option.innerText = 'New list';
+  const option = createOption(createID);
+  // Add option to DOM
   selectMenu.appendChild(option);
 }
 
+function createListItem(id) {
+  const task = document.createElement('li');
+  task.setAttribute('data-id', id);
+  return task;
+}
+
+function createDeleteItemButton(itemID) {
+  const span = document.createElement('span');
+  span.setAttribute('class', 'btns');
+
+  const removeBtn = document.createElement('i');
+  removeBtn.setAttribute('class', 'fas fa-times-circle removeBtn');
+  removeBtn.setAttribute('data-id', itemID);
+  span.appendChild(removeBtn);
+  return span;
+}
 function addTask(text) {
   let createID = dayjs().valueOf();
   // Create a task
-  const task = document.createElement('li');
-  task.setAttribute('data-id', createID);
+  const task = createListItem(createID);
   // Create the remove button
-  const span = document.createElement('span');
-  span.setAttribute('class', 'btns');
-  const removeBtn = document.createElement('i');
-  removeBtn.setAttribute('class', 'fas fa-times-circle removeBtn');
-  removeBtn.setAttribute('data-id', task.dataset.id);
+  const span = createDeleteItemButton(createID);
 
-  span.appendChild(removeBtn);
+  // Add to DOM
   task.textContent = text;
   task.appendChild(span);
-  removeBtn.addEventListener('click', function (e) {
-    const delBtn = e.target;
-    console.log(delBtn.parentElement.parentElement);
-  });
+
   if (text) return task;
 }
 submitBtn.onclick = function (e) {
@@ -72,8 +101,3 @@ submitBtn.onclick = function (e) {
 createListBtn.onclick = function () {
   createList();
 };
-
-if (deleteTaskBtn)
-  deleteTaskBtn.addEventListener('click', function (e) {
-    console.log(e.target);
-  });
