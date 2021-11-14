cfd427989003cfcc6ce3828046ec6c63531c8184
SvetozarMiskovic <svetomiskovic98@gmail.com>
2021-11-14 02:49:37 +0100

    added database

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
+// FIREBASE //
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+// FIREBASE CONFIG //
+const firebaseConfig = {
+  apiKey: 'AIzaSyA5nouHr1CvBjyjDFiEABT5cgzYh2zbCog',
+  authDomain: 'todo-list-e8735.firebaseapp.com',
+  databaseURL:
+    'https://todo-list-e8735-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'todo-list-e8735',
+  storageBucket: 'todo-list-e8735.appspot.com',
+  messagingSenderId: '237387626426',
+  appId: '1:237387626426:web:adbc0d01b6a1c3da460f83',
+  measurementId: 'G-27QDMXJPZZ',
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+console.log(app);
+console.log(db);
+// END OF FIREBASE CONFIG //
+
+// END OF FIREBASE IMPORTS //
 import './style.css';
 import { getElsAndAppend } from './getElsAndAppend';
 import { saveLS } from './saveLS';
